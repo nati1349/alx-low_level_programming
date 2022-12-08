9f77e5674a306642ddcb65f231779defc0c37533
nati1349 <natan1349@gmail.com>
2022-12-08 03:38:13 -0800

    4. Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-
-
-/*
- * main - Entry point*
- *    
- * Return: Always 0 (Success)
+/**
+ *
+ * main - main block
+ *
+ * Return: 0
  *
  */
 
@@ -13,8 +12,8 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-			return (0);
+	return (0);
 
 }
