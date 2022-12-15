6eba3aa489325f766d3112bf3fee3ca9256c1abf
nati1349 <natan1349@gmail.com>
2022-12-15 09:58:00 -0800

    Task11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,44 +1,22 @@
 #include <stdio.h>
 /**
- * main - Entry pont
+ * main - prints largest prime factor
+ * Return: Always 0
  */
 
-
-
-int main()
+int main(void)
 {
-    int i, j, isPrime;
-    long int num;
-    /* Input a number from user */
+	long int n, fp;
 
-  num = 612852475143;
-
-    printf("All Prime Factors of %ld are: \n", num);
-
-    /* Find all Prime factors */
-    for(i=2; i<=num; i++)
-    {
-        /* Check 'i' for factor of num */
-        if(num%i==0)
-        {
-            /* Check 'i' for Prime */
-            isPrime = 1;
-            for(j=2; j<=i/2; j++)
-            {
-                if(i%j==0)
-                {
-                    isPrime = 0;
-                    break;
-                }
-            }
-
-            /* If 'i' is Prime number and factor of num */
-            if(isPrime==1)
-            {
-                printf("%d, ", i);
-            }
-        }
-    }
-
-    return 0;
+	n = 612852475143;
+	for (fp = 2; fp <= n; fp++)
+	{
+		if (n % fp == 0)
+		{
+			n /= fp;
+			fp--;
+		}
+	}
+	printf("%ld\n", fp);
+	return (0);
 }
