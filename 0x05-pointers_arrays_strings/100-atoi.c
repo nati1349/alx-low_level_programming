cf44fedb01b9a831c388ec07bab901741d53cf9c
nati1349 <natan1349@gmail.com>
2022-12-19 03:46:51 -0800

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,31 @@
-/**
-* _atoi - function that convert a string to an integer.
-* @s: string
-* Return: integer
-*/
+#include "main.h"
 
+/**
+ * _atoi - Entry point
+ * Desc: Entry
+ *@s: pointer
+ * Return: Always 0 (Success)
+ */
 int _atoi(char *s)
 {
-	return (*s);
+	int nev = 1;
+	int i;
+	unsigned int num = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+		{
+			nev *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = (num * 10) + (s[i] - '0');
+		}
+		else if (num > 0)
+		{
+			break;
+		}
+	}
+	return (num * nev);
 }
