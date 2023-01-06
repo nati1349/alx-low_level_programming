5bab157dda2c34c9ef87b4dd008c63ca77508d7b
nati1349 <natan1349@gmail.com>
2023-01-06 02:09:30 -0800

    Updated

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,19 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
 
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ *
+ * @str: parameter defined in main
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
