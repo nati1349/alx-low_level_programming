bfdee8ddd4ee7e647f897eac12e627f33286ae04
nati1349 <natan1349@gmail.com>
2023-01-06 03:03:04 -0800

    Task 1

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,41 +1,24 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * _strcmp - function that compares two strings
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
  *
- * @s1: parameter defined in main
- * @s2: parameter defined in main
- *
- * Return: 15 or -15 or 0
+ * Return: s1[i] - s2[i]
  */
-
 int _strcmp(char *s1, char *s2)
 {
-/*
- *	char *tmp1 = s1;
- *	char *tmp2 = s2;
- */
-	while (*s1 != '\0')
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (*s1 == *s2)
+		if (s1[i] != s2[i])
 		{
-			s1++;
-			s2++;
+			return (s1[i] - s2[i]);
 		}
-		else
-		{
-			if (*s1 > *s2)
-				return (*s1 - *s2);
-			else if (*s1 < *s2)
-				return (*s1 - *s2);
-			s1++;
-			s2++;
-		}
+	i++;
 	}
-/*
- *	s1 = tmp1;
- *	s2 = tmp2;
- */
 	return (0);
 }
+
