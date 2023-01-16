65200a13e460d31752798a725a61fce2fe0fb6b1
nati1349 <natan1349@gmail.com>
2023-01-16 03:08:15 -0800

    update

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,6 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H
-#define _3_FUNCTION_LIKE_MACRO_H
+#ifndef _function_like_macro_h_
+#define _function_like_macro_h_
 
-#define ABS(X) ((X) < 0 ? (-X) : (X))
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
 
-#endif
-
+#endif /* _function_like_macro_h_ */
