5a59e53a69d8cf69b5718befd6f2516620ceee09
nati1349 <natan1349@gmail.com>
2023-02-01 09:58:10 -0800

    Updated

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,23 +1,53 @@
 #include "lists.h"
 
 /**
- * reverse_listint - prints a listint_t linked list.
- * @head: pointer to the list.
- * Return: number of nodes in the list.
- **/
-size_t print_listint_safe(const listint_t *head)
+ * interval - checks if the linked list has not looped
+ *
+ * @begin: the begining of the list
+ * @node: the current node
+ * @i: the interval that should be at current position
+ *
+ * Return: 1 if interval is correct, 0 if there is a loop
+ */
+
+size_t	interval(const listint_t *begin, const listint_t *node, size_t i)
 {
-	size_t pichu = 0;
-	const listint_t *aux_node = head;
+	size_t check = 0;
 
-	if (!head)
-		exit(98);
-
-	while (aux_node)
+	while (begin != node)
 	{
-		printf("[%p] %i\n", (void *)aux_node, aux_node->n);
-		aux_node = aux_node->next;
-		pichu++;
+		begin = begin->next;
+		check++;
 	}
-	return (pichu);
+
+	return ((check == i) ? 1 : 0);
+}
+
+/**
+ * print_listint_safe - prints a listint_t linked list
+ *
+ * @head: the listint_t argument (head)
+ *
+ * Return: the number of nodes in the list
+ */
+
+size_t	print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+	const listint_t *begin = head;
+
+	if (head)
+	{
+		while (head && interval(begin, head, count))
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+
+			count++;
+		}
+
+		if (head)
+			printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+	return (count);
 }
