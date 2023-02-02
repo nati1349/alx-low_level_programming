ccc9b42e1e18c2f41b4f9dc92bb2974a85d90bbb
nati1349 <natan1349@gmail.com>
2023-02-02 11:33:25 -0800

    Udated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,47 +1,33 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * binary_to_uint - Converts a binary number to an unsigned int
-  * @b: The binary string to converts
-  *
-  * Return: The positive number converted from a binary
-  */
+ * binary_to_uint - program startup
+ * @b: char being evaluated
+(*
+ * Description: converts a binary number to an unsigned int)?
+ * Return: return (0) is the required function signature
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len = 0, count = 0, sum = 0;
+	unsigned int num = 0;
+	int x;
 
 	if (b == NULL)
-		return (0);
-
-	len = _strlen(b);
-	while (len--)
 	{
-		if (b[len] != 48 && b[len] != 49)
-			return (0);
-
-		if (b[len] == 49)
-			sum += 1 << count;
-
-		count++;
+		return (0);
 	}
-
-	return (sum);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(const char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
+	for (x = 0; b[x] != '\0'; x++)
+	{
+		if (b[x] == '0' || b[x] == '1')
+		{
+			num <<= 1;
+			num += b[x] - '0';
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (num);
 }
