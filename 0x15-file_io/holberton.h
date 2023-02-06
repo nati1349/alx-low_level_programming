ea8b4fba075c0fe245aa2cd808304d45987ed119
nati1349 <natan1349@gmail.com>
2023-02-06 06:04:48 -0800

    Updated

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,6 +1,16 @@
-#ifndef __HOLBERTON_H__
-#define __HOLBERTON_H__
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
-#endif /* __HOLBERTON_H__ */
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <elf.h>
+
+int		_putchar(char c);
+
+ssize_t	read_textfile(const char *filename, size_t letters);
+int		create_file(const char *filename, char *text_content);
+int		append_text_to_file(const char *filename, char *text_content);
+
+#endif /* HOLBERTON_H */
