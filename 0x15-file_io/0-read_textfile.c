b07dab5d1765607a230faad9257046e3f4b77d52
nati1349 <natan1349@gmail.com>
2023-02-06 06:02:15 -0800

    Updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,35 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
+#include "holberton.h"
 
 /**
- * read_textfile - A function that reads a text file and prints
- * to the POSIX STDOUT
- * @filename: The filename to open
- * @letters: The number of letters to read and print
- * Return: The number of letters read and printed, or 0 on failure
+ * read_textfile - reads a text file ans prints it to the POSIX standard output
+ *
+ * @filename: the path of the file
+ * @letters: the number of letters it should read and print
+ *
+ * Return: number of letters it could read and print
+ * or 0 if the file can't be opened, filename is NULL, or write fails
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+
+ssize_t	read_textfile(const char *filename, size_t letters)
 {
-	int fdo, fdr, fdw;
-	char *temp;
+	int fd = open(filename, O_RDONLY);
+	ssize_t bytes = 0, write_o = -1;
+	char *buf = NULL;
 
-	if (filename == NULL)
-		return (0);
-
-	temp = malloc(sizeof(char) * letters);
-	if (temp == NULL)
-		return (0);
-
-	fdo = open(filename, O_RDONLY);
-	if (fdo < 0)
+	if (fd > -1 && filename)
 	{
-		free(temp);
-		return (0);
+		buf = malloc(sizeof(char) * (letters + 1));
+		if (buf)
+		{
+			bytes = read(fd, buf, letters);
+			buf[bytes] = '\0';
+			if (bytes > -1)
+				write_o = write(STDOUT_FILENO, buf, bytes);
+			free(buf);
+			close(fd);
+			if (write_o > -1)
+				return (bytes);
+		}
 	}
-
-	fdr = read(fdo, temp, letters);
-	if (fdr < 0)
-	{
-		free(temp);
-		return (0);
-	}
-
-	fdw = write(STDOUT_FILENO, temp, fdr);
-	free(temp);
-	close(fdo);
-
-	if (fdw < 0)
-		return (0);
-	return ((ssize_t)fdw);
+	return (0);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,30 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include "holberton.h"
 
 /**
- * create_file - A function that creates a file
- * @filename: The filename to create
- * @text_content: A NULL terminated string to write to the file
- * Return: 1 on success, -1 if file can not be created, nor written,
- * nor write fails.
+ * create_file - creates a file
+ *
+ * @filename: the path of the file
+ * @text_content: the content of the file to write in
+ *
+ * Return: 1 on success, -1 on failure (file can't be created, written)
  */
-int create_file(const char *filename, char *text_content)
+
+int	create_file(const char *filename, char *text_content)
 {
-	int fdo, fdw, len = 0;
+	int fd;
+	int len = 0;
+	ssize_t write_o = 0;
 
-	if (filename == NULL)
-		return (-1);
-
-	fdo = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fdo < 0)
-		return (-1);
-
-	while (text_content && *(text_content + len))
-		len++;
-
-	fdw = write(fdo, text_content, len);
-	close(fdo);
-	if (fdw < 0)
-		return (-1);
-	return (1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd > -1 && filename)
+	{
+		while (text_content && text_content[len])
+			len++;
+		if (len > 0)
+			write_o = write(fd, text_content, len);
+		close(fd);
+		if (write_o > -1)
+			return (1);
+	}
+	return (-1);
 }
